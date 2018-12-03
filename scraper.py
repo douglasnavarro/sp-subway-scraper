f692428da88c60e4152b7239d33932f4bfdd9a80
Douglas Navarro <douglasnavarro94@gmail.com>
2018-12-02 22:21:41 -0200

    update sheet for december

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ lines_metro = ['azul', 'verde', 'vermelha', 'amarela', 'lilás', 'prata']
 lines_cptm  = ['rubi', 'diamante', 'esmeralda', 'turquesa', 'coral', 'safira', 'jade']
 all_lines   = lines_metro + lines_cptm
 
-SPREADSHEET_ID = "12mzoEpbjmDFVJnzfjyK1YOBRSWO8e2XChHANEWJMQFo"
+SPREADSHEET_ID = "1J29Ftwp-lm56V8-O0pKZo_zQgpLya-wFfz2L49FGkvg"
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
