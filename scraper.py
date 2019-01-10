a795c7f9102926b2556830c83c180ea23ca81e0a
Douglas Navarro <douglasnavarro94@gmail.com>
2019-01-09 22:16:15 -0200

    atualiza p/ janeiro 2019

--- scraper.py
+++ scraper.py
@@ -1,23 +1,29 @@
-import requests
-import re
-from bs4 import BeautifulSoup
-import time
 import logging
+import os
+import requests
+from bs4 import BeautifulSoup
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import os
 from email_debug import send_email as send_email
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-lines_metro = ['azul', 'verde', 'vermelha', 'amarela', 'lilás', 'prata']
-lines_cptm  = ['rubi', 'diamante', 'esmeralda', 'turquesa', 'coral', 'safira', 'jade']
-all_lines   = lines_metro + lines_cptm
+LINES_METRO = ['azul', 'verde', 'vermelha', 'amarela', 'lilás', 'prata']
+LINES_CPTM = [
+    'rubi',
+    'diamante',
+    'esmeralda',
+    'turquesa',
+    'coral',
+    'safira',
+    'jade']
+ALL_LINES = LINES_METRO + LINES_CPTM
 
-SPREADSHEET_ID = "1J29Ftwp-lm56V8-O0pKZo_zQgpLya-wFfz2L49FGkvg"
+SPREADSHEET_ID = "1OnDkKFvaVPaRGRUURK6KXla0AIQnpDj-wEBx4rPntP0"
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 logger.info('Starting scraper')
+
 
 def get_page_html(url):
     try:
@@ -26,35 +32,37 @@ def get_page_html(url):
             return page.text
         else:
             return None
-    except:
+    except BaseException:
         return None
+
 
 def init_sheet(SPREADSHEET_ID):
     # use creds to create a client to interact with the Google Drive API
     scope = ['https://spreadsheets.google.com/feeds',
-            'https://www.googleapis.com/auth/drive']
+             'https://www.googleapis.com/auth/drive']
     headers = {
-            "type": os.environ.get('TYPE', None),
-            "project_id": os.environ.get('PROJECT_ID', None),
-            "private_key_id": os.environ.get('PRIVATE_KEY_ID', None),
-            "private_key": os.environ.get('PRIVATE_KEY', None),
-            "client_email": os.environ.get('CLIENT_EMAIL', None),
-            "client_id": os.environ.get('CLIENT_ID', None),
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://accounts.google.com/o/oauth2/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": os.environ.get('CLIENT_x509_CERT_URL', None)
-        }
-    #logger.info(str(headers))
+        "type": os.environ.get('TYPE', None),
+        "project_id": os.environ.get('PROJECT_ID', None),
+        "private_key_id": os.environ.get('PRIVATE_KEY_ID', None),
+        "private_key": os.environ.get('PRIVATE_KEY', None),
+        "client_email": os.environ.get('CLIENT_EMAIL', None),
+        "client_id": os.environ.get('CLIENT_ID', None),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ.get('CLIENT_x509_CERT_URL', None)
+    }
+    # logger.info(str(headers))
     creds = ServiceAccountCredentials.from_json_keyfile_dict(headers, scope)
     #creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
     client = gspread.authorize(creds)
     data_sheet = client.open_by_key(SPREADSHEET_ID).worksheet("data")
     return data_sheet
 
+
 def get_operation_status(soup, all_lines):
 
-    extracted_status = {line:'' for line in all_lines}
+    extracted_status = {line: '' for line in all_lines}
 
     # Contains all the info we need
     status_column = soup.find(class_="operacao")
@@ -62,27 +70,32 @@ def get_operation_status(soup, all_lines):
     # The 'amarela' line is shown in a special container
     extracted_status['amarela'] = status_column.find(class_="status").text
 
-    # All of the other lines are shown in a more orderly fashion. Metro has a div and CPTM has another one
-    lines_containers  = status_column.find_all(class_="linhas")
+    # All of the other lines are shown in a more orderly fashion. Metro has a
+    # div and CPTM has another one
+    lines_containers = status_column.find_all(class_="linhas")
 
     for container in lines_containers:
         line_info_divs = container.find_all(class_="info")
-        # each info div has two span tags inside: one for line title and one for line status
+        # each info div has two span tags inside: one for line title and one
+        # for line status
         for div in line_info_divs:
-            line_title  = ''
+            line_title = ''
             line_status = ''
             spans = div.find_all("span")
             line_title = spans[0].text.lower()
             line_status = spans[1].text.lower()
-            # now that we have line_title and line_status set, we only have to store it to return later
+            # now that we have line_title and line_status set, we only have to
+            # store it to return later
             extracted_status[line_title] = line_status
         logging.info('Extracted: {}'.format(extracted_status))
 
     return(extracted_status)
 
+
 def get_time_data(soup):
 
     return soup.find('time').text
+
 
 def check_data(op_status, page):
     for status in op_status.values():
@@ -90,8 +103,11 @@ def check_data(op_status, page):
             send_email(page)
             break
 
+
 sched = BlockingScheduler()
 args = [SPREADSHEET_ID, all_lines]
+
+
 @sched.scheduled_job('interval', minutes=6, args=args)
 def timed_job(SPREADSHEET_ID, all_lines):
     vq_home = get_page_html('http://www.viaquatro.com.br')
@@ -107,5 +123,6 @@ def timed_job(SPREADSHEET_ID, all_lines):
 
     for line in all_lines:
         data_sheet.append_row([time_data, line, op_status[line]])
+
 
 sched.start()
