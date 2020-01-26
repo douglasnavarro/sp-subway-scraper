1d32e6d5fd2740c1e70e0915f492343f5de28752
Douglas Navarro <douglas.navarro@nubank.com.br>
2020-01-26 18:02:59 -0300

    lint fix

--- email_debug.py
+++ email_debug.py
@@ -6,11 +6,12 @@ from email.mime.multipart import MIMEMultipart
 
 COMMASPACE = ', '
 
+
 def send_email(attachment):
 
-    sender = os.environ.get('SENDER', None) 
-    gmail_password  = os.environ.get('GMAIL_PASSWORD', None)
-    recipients   = ['douglasnavarro94@gmail.com']
+    sender = os.environ.get('SENDER', None)
+    gmail_password = os.environ.get('GMAIL_PASSWORD', None)
+    recipients = ['douglasnavarro94@gmail.com']
 
     outer = MIMEMultipart()
     outer['Subject'] = 'Debug scraper'
@@ -18,18 +19,11 @@ def send_email(attachment):
     outer['From'] = sender
     outer.preamble = 'You will not see this in a MIME-aware mail reader.\n'
 
-    # msg = "\r\n".join([
-    # "From: sp.subway.scraper@gmail.com",
-    # "To: douglasnavarro94@gmail.com",
-    # "Subject: sp-subway-scraper-debug",
-    # "",
-    # body
-    # ])
-
     msg = MIMEBase('application', "octet-stream")
     msg.set_payload(attachment)
     encoders.encode_base64(msg)
-    msg.add_header('Content-Disposition', 'attachment', filename='viaquatro.html')
+    msg.add_header('Content-Disposition', 'attachment',
+                   filename='viaquatro.html')
     outer.attach(msg)
 
     composed = outer.as_string()
